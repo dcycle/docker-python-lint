f77a855a58957d48b5e6592d01a9e6a93e98a19a
Albert Albala <albert@mediatribe.net>
2021-07-17 17:43:07 -0400

    Please squash this commit in the pull request

--- test.sh
+++ test.sh
@@ -1,9 +1,6 @@
 set -e
-docker pull node
-docker pull dcycle/python-lint
+docker pull ubuntu
 docker build -t local-dcycle-python-lint-image .
 
-docker run -v $(pwd)/example:/app/code dcycle/python-lint --help
-docker run -v $(pwd)/example:/app/code dcycle/python-lint .
 docker run -v $(pwd)/example:/app/code local-dcycle-python-lint-image --help
 docker run -v $(pwd)/example:/app/code local-dcycle-python-lint-image .
