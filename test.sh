074e919d42598326843a2e448c71d49db21df7e1
Albert Albala <albert@mediatribe.net>
2021-07-17 17:55:20 -0400

    Fix failing tests (#1)

--- test.sh
+++ test.sh
@@ -1,9 +1,17 @@
 set -e
-docker pull node
-docker pull dcycle/python-lint
+docker pull ubuntu
 docker build -t local-dcycle-python-lint-image .
 
-docker run -v $(pwd)/example:/app/code dcycle/python-lint --help
-docker run -v $(pwd)/example:/app/code dcycle/python-lint .
-docker run -v $(pwd)/example:/app/code local-dcycle-python-lint-image --help
-docker run -v $(pwd)/example:/app/code local-dcycle-python-lint-image .
+echo "=>"
+echo "=> Running help command"
+echo "=>"
+docker run --rm -v $(pwd)/example:/app/code local-dcycle-python-lint-image --help
+
+echo "=>"
+echo "=> Running pylint on the example"
+echo "=>"
+docker run --rm -v $(pwd)/example:/app/code local-dcycle-python-lint-image ./code
+
+echo "=>"
+echo "=> All done"
+echo "=>"
