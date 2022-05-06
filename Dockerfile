d1ab919e6a13939af55cd5b27fcd69fece900ae1
Albert Albala <albert@mediatribe.net>
2022-05-06 12:58:12 -0400

    Please squash this commit in the pull request

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM ubuntu
+# https://stackoverflow.com/questions/43764037/install-pylint-in-alpine-linux-based-docker-image
+FROM python:3-alpine
 
 RUN mkdir -p /app/code
 
-RUN apt-get -y update
-RUN apt-get -y install pylint
+RUN pip3 install -U pylint
 
 WORKDIR /app
 
--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -16,21 +16,29 @@ else
   source "$CREDENTIALS";
 fi
 
+./test.sh
+
 PROJECT=python-lint
 DATE=`date '+%Y-%m-%d-%H-%M-%S-%Z'`
-MAJORVERSION='1'
-VERSION='1.0'
+MAJORVERSION='2'
+VERSION='2.0'
 
-# Start by getting the latest version of the official drupal image
-docker pull ubuntu
-# Rebuild the entire thing
-docker build --no-cache -t dcycle/"$PROJECT":"$VERSION" .
-docker build -t dcycle/"$PROJECT":"$MAJORVERSION" .
-docker build -t dcycle/"$PROJECT":"$MAJORVERSION".$DATE .
-docker build -t dcycle/"$PROJECT":"$VERSION".$DATE .
+# Start by getting the latest version of the official base image
+docker pull python:3-alpine
+
+# See https://github.com/dcycle/prepare-docker-buildx, for M1 native images.
+git clone https://github.com/dcycle/prepare-docker-buildx.git
+cd prepare-docker-buildx
+export DOCKER_CLI_EXPERIMENTAL=enabled
+./scripts/run.sh
+cd ..
+
+docker buildx create --name mybuilder
+docker buildx use mybuilder
+docker buildx inspect --bootstrap
 docker login -u"$DOCKERHUBUSER" -p"$DOCKERHUBPASS"
-docker push dcycle/"$PROJECT":"$VERSION"
-docker push dcycle/"$PROJECT":"$MAJORVERSION"
-docker push dcycle/"$PROJECT":"$VERSION"."$DATE"
-docker push dcycle/"$PROJECT":"$MAJORVERSION"."$DATE"
-# No longer using the latest tag, use the major version tag instead.
+
+docker buildx build -t dcycle/"$PROJECT":"$VERSION" --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -t dcycle/"$PROJECT":"$MAJORVERSION" --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -t dcycle/"$PROJECT":"$MAJORVERSION".$DATE --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -t dcycle/"$PROJECT":"$VERSION".$DATE --platform linux/amd64,linux/arm64/v8 --push .
